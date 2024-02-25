753f7a06d5875967890cf086db1bd20179f1e350
Yusarina <hello@yusarina.xyz>
2024-02-25 01:50:39 +0000

    Add options to export shapekeys tanslations to a csv
    This adds a new setting in update and settings called when checked CATS will export shape keys translations to a CSV file located in Documents - cats - shapekeys.csv
    
    This is to complete #51  feature request. This will allow users to easily fix animations and such in Unity as there may break when translating shapekeys as Unity relies on the shapekey name and if that changes it will break the anmination.

--- ui/settings_updates.py
+++ ui/settings_updates.py
@@ -30,6 +30,8 @@ class UpdaterPanel(ToolPanel, bpy.types.Panel):
         row.prop(context.scene, 'show_mmd_tabs')
         row = col.row(align=True)
         row.prop(context.scene, 'embed_textures')
+        row = col.row(align=True)
+        row.prop(context.scene, "export_shapekeys_csv")
 
         col.separator()
 
