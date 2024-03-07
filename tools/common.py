5d8af3b1ef7eb6b6bc95b28b816481db444d7ceb
Yusarina <hello@yusarina.xyz>
2024-03-07 16:55:14 +0000

    fix to issue #64
    Removed check if threshold <= 0: for remove doubles, user can't set own threshold so no point in having it.

--- tools/common.py
+++ tools/common.py
@@ -1394,8 +1394,6 @@ def _get_shape_key_co(shape_key: ShapeKey) -> np.ndarray:
 def remove_doubles(mesh_obj: Object, threshold: float, save_shapes: bool = True) -> int:
     if not isinstance(mesh_obj, Object):
         raise TypeError("mesh_obj must be an instance of Object")
-    if threshold <= 0:
-        raise ValueError("Threshold must be positive")
 
     mesh = mesh_obj.data
 
