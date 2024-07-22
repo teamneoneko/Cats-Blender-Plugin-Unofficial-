973881b56ee78b48e77bd8dd766a6f0e787f7bb5
Yusarina <hello@yusarina.xyz>
2024-07-22 23:49:47 +0100

    Version Bump

--- __init__.py
+++ __init__.py
@@ -168,14 +168,14 @@ def remove_corrupted_files():
 
 
 def check_unsupported_blender_versions():
-    # Don't allow Blender versions older than 4.1
-    if bpy.app.version < (4, 2):
+    # Don't allow Blender versions older than 4.2
+    if bpy.app.version < (4, 3):
         unregister()
         sys.tracebacklimit = 0
         raise ImportError(t('Main.error.29unsupportedVersion'))
      
-    # Don't allow 4.3+
-    if bpy.app.version >= (4, 3):
+    # Don't allow 4.4+
+    if bpy.app.version >= (4, 4):
         sys.tracebacklimit = 0
         raise ImportError(t('Main.error.40unsupportedVersion'))
 
