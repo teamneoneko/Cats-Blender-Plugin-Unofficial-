33838f9309f67f0839fcf599a277a681a86a541b
Yusarina <hello@yusarina.xyz>
2024-07-22 03:51:49 +0100

    Added Missing Strings
    - There was ALOT of missing strings from the translation update this adds them back.

--- extentions.py
+++ extentions.py
@@ -388,26 +388,6 @@ def register():
             ("BONEMERGING", t('Scene.optimize_mode.bonemerging.label'), t('Scene.optimize_mode.bonemerging.desc')),
         ]
     )
-
-    # Atlas
-    # Material.add_to_atlas = BoolProperty(
-    #     description=t('Add this material to the atlas'),
-    #     default=False
-    # )
-
-    # Scene.material_list_index = IntProperty(
-    #     default=0
-    # )
-
-    # Scene.material_list = CollectionProperty(
-    #     type=Atlas.MaterialsGroup
-    # )
-
-    # Scene.clear_materials = BoolProperty(
-    #     description=t('Clear materials checkbox'),
-    #     default=True
-    # )
-
     # Bone Merging
     Scene.merge_ratio = FloatProperty(
         name=t('Scene.merge_ratio.label'),
