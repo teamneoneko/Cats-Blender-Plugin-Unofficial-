ba37e7fc2cb621ee0771cd79145d5f803666d272
Yusarina <hello@yusarina.xyz>
2024-07-22 17:13:55 +0100

    Make sure we use Blender 4.1 Dictionary

--- tools/translations.py
+++ tools/translations.py
@@ -24,7 +24,7 @@ translations_dir = os.path.join(resources_dir, "translations")
 dictionary: dict[str, str] = dict()
 languages = []
 verbose = True
-dictionary_download_link = "https://raw.githubusercontent.com/teamneoneko/Cats-Blender-Plugin-Unofficial-translations/3.6-4.0-translations/dictionary.json"
+dictionary_download_link = "https://raw.githubusercontent.com/teamneoneko/Cats-Blender-Plugin-Unofficial-translations/4.1-translations/dictionary.json"
 
 def load_translations():
     global dictionary, languages
